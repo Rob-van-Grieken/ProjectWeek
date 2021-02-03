32fbc9a9e1bda988848be741c223e1b371b8194c
Rob-van-Grieken <331665@student.mboutrecht.nl>
2021-02-03 11:56:57 +0100

    unknown

--- project-week/Assets/scripts/molesSpawner.cs
+++ project-week/Assets/scripts/molesSpawner.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class molesSpawner : MonoBehaviour
 {
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //causes the random spawn time of the animals
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void SpawnRandomAnimal()
+    {
+        //causes random location for the animals to spawn
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
     }
 }
